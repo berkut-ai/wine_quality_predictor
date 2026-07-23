642f9e581a0367c8d9aec9b7ff5faf21466a480e
root <>
2026-07-23 17:32:43 +0200

    added request limit in main, updated

--- main.py
+++ main.py
@@ -3,18 +3,26 @@ from logging_config import setup_logging
 
 setup_logging()
 
-from fastapi import FastAPI, Body, HTTPException
+from fastapi import FastAPI, Body, HTTPException, Request
 from inference import predict
 from time import perf_counter
 from typing import Annotated
 from schemas import PredictResponse, PredictRequest, Wine
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.errors import RateLimitExceeded
+from slowapi.util import get_remote_address
 
 logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
+limiter = Limiter(key_func=get_remote_address)
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
+
 @app.post('/predict', response_model=PredictResponse)
-async def create_predict(data: Annotated[Wine, Body()]):
+@limiter.limit('30/minute')
+async def create_predict(request: Request, data: Annotated[Wine, Body()]):
     wine = f"{data.name}"
 
     logger.info(f"Accepted request for wine: {wine}.")
